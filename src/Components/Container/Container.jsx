4386237ea4aa60cb5aa2e92f8f481f8924dd229e
ayon008 <shariar.ayon128@gmail.com>
2023-09-23 17:04:51 +0600

    toast-added

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './Container.css';
 import Blog from '../Blogs/Blog';
 import Bookmarks from '../Bookmarks/Bookmarks';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Container = () => {
     const [blogs, setBlogs] = useState([])
@@ -18,8 +19,13 @@ const Container = () => {
     const buttonHandle = blog => {
         let newBlogLists = [...blogLists, blog];
         const matched = newBlogLists.find(newBlogList => newBlogList.id === blog.id);
+        const notify = () => toast("❌ Bookmarks all ready added");
         const remaining = newBlogLists.filter(newBlogList => newBlogList.id !== blog.id);
         let uniqueBlogList = [...remaining, matched];
+        const matched2 = blogLists.find(blogList => blogList.id === blog.id);
+        if (matched2) {
+            notify();
+        }
         setBlogLists(uniqueBlogList);
     }
 
@@ -29,9 +35,14 @@ const Container = () => {
         const existing = newArray.find(singleTime => singleTime.id === passedBlog.id);
         const remaining = newArray.filter(singleTime => singleTime.id !== passedBlog.id);
         newArray = [...remaining, existing];
+        const matched = timeArray.find(time => time.id === passedBlog.id)
+        const notify = () => toast("❌ all ready read");
+        if (matched) {
+            notify();
+        }
         setTimeArray(newArray);
     }
-   
+
     return (
         <div className='container'>
             <div className='blogs'>
@@ -42,6 +53,7 @@ const Container = () => {
             <div className='bookmarks'>
                 <Bookmarks showTime={timeArray} blogs={blogLists}></Bookmarks>
             </div>
+            <ToastContainer />
         </div>
     );
 };
